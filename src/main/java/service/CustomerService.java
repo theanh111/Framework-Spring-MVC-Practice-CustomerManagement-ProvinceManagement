ea27a34584a6b637ee5b59159b670077fba38604
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2020-11-25 17:24:30 +0700

    [Thực hành] Ứng dụng Quản lý khách hàng: Quản lý tỉnh

---
+++ src/main/java/service/CustomerService.java
@@ -0,0 +1,17 @@
+package service;
+
+
+import model.Customer;
+import model.Province;
+
+public interface CustomerService {
+    Iterable<Customer> findAll();
+
+    Customer findById(Long id);
+
+    void save(Customer customer);
+
+    void remove(Long id);
+
+    Iterable<Customer> findAllByProvince(Province province);
+}
